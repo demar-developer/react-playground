c5bd2733418873e2be558053016aafe556092dd6
Demar <demar.developer@gmail.com>
2023-02-24 10:02:10 -0500

    Adding el in Array

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,46 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cells, setCells] = useState(["a", "b", "c"])
+
+  const changeCellContent = (newCellValue: string, indexToUpdate: number) => {
+    setCells((prevCells) => (
+      prevCells.map((cell, index) => (index === indexToUpdate ? newCellValue : cell))
+    ))
+  }
+
+  const handlePlusClicked = (index: number) => {
+    setCells((prevCells) => {
+      const newCells = [...prevCells]
+      newCells.splice(index + 1, 0, "_")
+      return newCells
+    })
+  }
+
+  const combineCells = cells.join('')
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <main className="App">
+      <section className="cells">
+        {cells.map((cell, index) => (
+          <div className="cell" key={index}>
+            <input
+              onChange={(e) => changeCellContent(e.currentTarget.value, index)}
+              value={cell}
+            ></input>
+            {index !== cells.length - 1 && (
+              <span
+                className="plus"
+                onClick={() => handlePlusClicked(index)}
+              ></span>
+            )}
+          </div>
+        ))}
+      </section>
+      <section className="cells-text">{combineCells}</section>
+    </main>
+  );
 }
 
 export default App
